51ef8fa6b576e9befe52a2f76531808235d72b26
AnaliaGiachini <analiagiachini@gmail.com>
2023-03-03 23:11:51 -0300

    V03

--- back/src/server.js
+++ back/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 /* Levanto el server en package json instalando nodemon */
+
 http.createServer((req,res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     if(req.url.includes('rickandmorty/character')) {
